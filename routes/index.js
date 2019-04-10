644e01ac8fd0d43a095a995b0eff2408946b0c24
Valentina Pogudina <valentina.pogudina@stonybrook.edu>
2019-04-10 16:17:27 -0400

    playing with sql query

--- routes/index.js
+++ routes/index.js
@@ -28,9 +28,7 @@ router.post('/hw7', function(req, res, next) {
 
   con.connect(function(err) {
     if (err) throw err;
-    var sql = 'SELECT as1.club, as1.pos, as1.player, as1.a, as1.gs ' +
-        'FROM assists as1 WHERE club = ? AND pos = ? AND ' +
-        'a = (select max(a) from assists as2 where as1.club = as2.club)';
+    var sql = 'SELECT club, pos, player, a, gs FROM assists WHERE club = ? AND pos = ? ORDER BY a, gs DESC limit 1;';
     con.query(sql, [club,pos],function (err, result, fields) {
       if (err) throw err;
       console.log('>> result: ', result );
